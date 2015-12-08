215bf0db6cde6f7e96a69278f9d3fdc5475b3ede
HB <HB@HB-PC>
2015-12-08 11:08:07 +0900

    ..

--- hnb/src/main/java/com/hnb/member/MemberController.java
+++ hnb/src/main/java/com/hnb/member/MemberController.java
@@ -1,5 +1,12 @@
 package com.hnb.member;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.hnb.main.MainController;
+
 public class MemberController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(MemberController.class);
 
 }
