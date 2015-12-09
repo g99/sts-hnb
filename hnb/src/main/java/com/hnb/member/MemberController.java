04b1bf19d88f3e122514997e52fc30de8c62c254
HB <HB@HB-PC>
2015-12-09 17:44:33 +0900

    ..

--- hnb/src/main/java/com/hnb/member/MemberController.java
+++ hnb/src/main/java/com/hnb/member/MemberController.java
@@ -2,21 +2,117 @@ package com.hnb.member;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import com.hnb.main.MainController;
+
 
 @Controller
 @RequestMapping("/member")
 public class MemberController {
 	private static final Logger logger = LoggerFactory.getLogger(MemberController.class);
 	
+	@Autowired
+	MemberServiceImpl service;
+	@Autowired
+	MemberVO member;
+	int result;
+
+	@RequestMapping("/admin_home")
+	public String adminHome(Model model){
+		logger.info("어드민홈 입장!!!!");
+		return "member/admin_home";
+	}
+	@RequestMapping("/provision")
+	public String provision(MemberVO member){
+		return "member/provision";
+	}
 	@RequestMapping("/join_member")
-	public String joinMember(MemberVO member){
+	public String joinMember(String id,String password,String name, String birth, String addr,
+			String gender,String email,String phone, Model model
+			){
+		logger.info("가입 아이디 : {}", id);
+		logger.info("가입 패스워드 : {}", password);
+		logger.info("가입 이름 : {}", name);
+		logger.info("가입 생년 : {}", birth);
+		logger.info("가입 성별 : {}", addr);
+		logger.info("가입 주소 : {}", gender);
+		logger.info("가입 이메일 : {}", email);
+		logger.info("가입 전화 번호 : {}", phone);
+		
+		member.setId(id);
+		member.setPassword(password);
+		member.setName(name);
+		member.setBirth(birth);
+		member.setAddr(addr);
+		member.setGender(gender);
+		member.setEmail(email);
+		member.setPhone(phone);
+		
+	    result = service.join(member);
+		if (result == 1) {
+			logger.info("회원가입 성공");
+			model.addAttribute("result", "success");
+			model.addAttribute("name", member.getName());
+			logger.info("결과 {}", model);
+		} else {
+			logger.info("회원가입 실패");
+			model.addAttribute("result", "fail");
+		}
 		return "member/join_member";
 	}
-
+	@RequestMapping("/join_Result")
+	public String joinResult(){
+		return "member/join_Result";
+	}
+	@RequestMapping("/logout")
+	public String logout(Model model){
+		logger.info("Member : 로그아웃 진입");
+		model.addAttribute("result", "success");
+		return "member/logout";
+	}
+	@RequestMapping("/login")
+	public String login(String id,String password, Model model
+			){
+		logger.info("Member : 로그인 진입");
+		logger.info("유저아이디 : " + id);
+		logger.info("유저패스워드 : " + password);
+		member = service.login(id, password);
+		if (member == null) {
+			model.addAttribute("result", "fail");
+		} else {
+			model.addAttribute("result", "success");
+			model.addAttribute("id", id);
+			model.addAttribute("pw", password);
+			if (id.equals("choa")) {
+				model.addAttribute("admin", "yes");
+			} else {
+				model.addAttribute("admin", "no");
+			}
+		}
+		return "member/login";
+	}
+	@RequestMapping("/check_Overlap")
+	public String checkOverlap(String id, Model model
+			){
+		logger.info("컨트롤러 / 중복체크로 진입");
+		if (service.searchById(id).getId() == null) {
+			model.addAttribute("result", "usable");
+			model.addAttribute("id", id);
+		} else {
+			model.addAttribute("result", "unusable");
+			model.addAttribute("id", id);
+		}
+		return "member/check_Overlap";
+	}
+	@RequestMapping("/mypage")
+	public String mypage(MemberVO member){
+		return "member/mypage";
+	}
+	@RequestMapping("/detail")
+	public String detail(MemberVO member){
+		return "member/detail";
+	}
 }
--- hnb/src/main/java/com/hnb/movie/MovieController.java
+++ hnb/src/main/java/com/hnb/movie/MovieController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/movie")
@@ -22,68 +23,64 @@ public class MovieController {
 	MovieVO movie;
 
 	@RequestMapping("/Movie")
-	public String home(){
-		logger.info("AdminController-home() 진입");
-		return "movie/Movie";
+	public String movie(){
+		logger.info("MovieController-Movie() 진입");
+		return "movie/Movie.tiles";
 	}
 	@RequestMapping("/movie_info")
-	public String movieInfo(Model model){
-		logger.info("AdminController-movieInfo() 진입");
+	public Model movieInfo(Model model){
+		logger.info("MovieController-movieInfo() 진입");
 		List<MovieVO> list = service.getList();
 		model.addAttribute("movieList", list);
 		logger.info("영화 리스트 조회결과 : {}", list);
-		return "movie/movie_info";
+		return model;
 	}
 	@RequestMapping("/movie_name/{movieName}")
-	public String movieName(
-			@PathVariable ("movieName")String name, Model model){
-		logger.info("AdminController-movieName() 진입");
+	public @ResponseBody MovieVO movieName(
+			@PathVariable ("movieName")String name){
+		logger.info("MovieController-movieName() 진입");
 		logger.info("영화 아이디 : {}", name);
 		movie = service.searchByName(name);
 		logger.info("영화제목 : {}", movie.getFilmName());
-		model.addAttribute("movie", movie);
-		return "movie/movie_name";
+		return movie;
 	} 
 	@RequestMapping("/movie_Cut")
-	public String movieCut(String filmNumber, Model model){
-		logger.info("AdminController-movieCut() 진입");
+	public Model movieCut(String filmNumber, Model model){
+		logger.info("MovieController-movieCut() 진입");
 		logger.info("영화 스틸컷 : {}", filmNumber);
 		movie = service.searchByName(filmNumber);
 		String cut = movie.getCut();
 		String[]arr = cut.split("/");
-		for (int i = 0; i < arr.length; i++) {
-			logger.info(arr[i]);
-		}
+		logger.info("배열결과 : {}", arr);
 		model.addAttribute("arr", arr);
-		return "movie/movie_Cut";
+		return model;
 	}
 	@RequestMapping("/movie_Tra")
 	public String movieTra(String filmNumber, Model model){
-		logger.info("AdminController-movieTra() 진입");
+		logger.info("MovieController-movieTra() 진입");
 		logger.info("영화 트레일러 : {}", filmNumber);
 		movie = service.searchByName(filmNumber);
 		String tra = movie.getTrailer();
 		logger.info("get 트레일러 : {}", tra);
 		String[]arrt = tra.split("/");
-		for (int i = 0; i < arrt.length; i++) {
-			logger.info(arrt[i]);
-		}
+		logger.info("트레일러 : ", arrt);
 		model.addAttribute("arrt", arrt);
 		return "movie/movie_Tra";
 	}
 	@RequestMapping("/movie_Basic")
-	public String movieBasic(String filmNumber, Model model){
-		logger.info("AdminController-movieBasic() 진입");
+	public @ResponseBody MovieVO movieBasic (String filmNumber, Model model){
+		logger.info("MovieController-movieBasic() 진입");
 		logger.info("movieBasic의 film넘버 : {}", filmNumber);
 		movie = service.searchByName(filmNumber);
 		logger.info("movieBasic컷의영화제목 : {} ", movie.getFilmName());
 		model.addAttribute("movie", movie);
-		return "movie/movie_Basic";
+		return movie;
 	}
 	@RequestMapping("/movie_Chart")
 	public String movieChart(String filmNumber, Model model){
-		logger.info("AdminController-movieChart() 진입");
-		
+		logger.info("MovieController-movieChart() 진입");
+		List<MovieVO> list = service.getList();
+		model.addAttribute("movieList2", list);
 		return "movie/movie_Chart";
 	}
 }
